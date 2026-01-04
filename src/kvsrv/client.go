d1af160760d558e83e7f70f4c7e0d51e77972831
Stef Robbe <stef.robbe@protonmail.com>
2026-01-04 15:44:27 +0100

    feat(ksrv): reliable network done
    Signed-off-by: Stef Robbe <stef.robbe@protonmail.com>

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -1,9 +1,11 @@
 package kvsrv
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
 
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
 	server *labrpc.ClientEnd
@@ -35,9 +37,17 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
+	args := GetArgs{Key: key}
+	reply := GetReply{}
 
-	// You will have to modify this function.
-	return ""
+	// for ok := ck.server.Call("KVServer.Get", &args, &reply); !ok; {
+	// 	fmt.Println("retrying GET")
+	// }
+
+	// TODO what to do with boolean ret?
+	_ = ck.server.Call("KVServer.Get", &args, &reply)
+
+	return reply.Value
 }
 
 // shared by Put and Append.
@@ -49,12 +59,17 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
-	// You will have to modify this function.
-	return ""
+	args := PutAppendArgs{Key: key, Value: value}
+	reply := PutAppendReply{}
+
+	// TODO what to do with boolean ret?
+	_ = ck.server.Call("KVServer."+op, &args, &reply)
+
+	return reply.Value
 }
 
 func (ck *Clerk) Put(key string, value string) {
-	ck.PutAppend(key, value, "Put")
+	_ = ck.PutAppend(key, value, "Put")
 }
 
 // Append value to key's value and return that value
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -1,43 +1,57 @@
 package kvsrv
 
 import (
+	"fmt"
 	"log"
 	"sync"
 )
 
 const Debug = false
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
+func DPrintf(format string, a ...any) (n int, err error) {
 	if Debug {
 		log.Printf(format, a...)
 	}
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	store map[string]string
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	v, _ := kv.store[args.Key]
+	reply.Value = v
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.store[args.Key] = args.Value
+	// TODO return value?
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	old, _ := kv.store[args.Key]
+	kv.store[args.Key] = fmt.Sprintf("%s%s", old, args.Value)
+	reply.Value = old
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
 
 	// You may need initialization code here.
+	kv.store = make(map[string]string)
 
 	return kv
 }
